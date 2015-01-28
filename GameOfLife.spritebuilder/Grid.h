ab39c943f327965217e63c8318a4a38d3a23bf92
Xintong Yu <yxt423@gmail.com>
2015-01-27 20:19:08 -0800

    Automatically Committed for MakeGamesWithUs

--- GameOfLife.spritebuilder/Grid.h
+++ GameOfLife.spritebuilder/Grid.h
@@ -13,10 +13,12 @@
 @property (nonatomic, assign) int totalAlive;
 @property (nonatomic, assign) int generation;
 
-- (void)evolveStep;
+- (void) evolveStep;
 
 - (void) countNeighbors;
 
 - (void) updateCreatures;
+
+- (BOOL) isIndexValidForX:(int)x andY:(int)y;
 
 @end
